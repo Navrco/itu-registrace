95c914cf008b1771cd9f8e46df1f97c8ed0dc29b
Navratil Rostislav <xnavra72@eva.fit.vutbr.cz>
2023-11-01 20:30:10 +0100

    lecture time span

--- frontend/src/components/RegisterForm.svelte
+++ frontend/src/components/RegisterForm.svelte
@@ -8,8 +8,70 @@
 	let lecturesData = false;
 
 
+	class rowOffsetCounter {
+		constructor(){
+			this.clearRows();
+		}
+
+		clearRows(){
+			delete this.rows;
+			this.rows = {
+				"Po": [this.createRow()],
+				"Út": [this.createRow()],
+				"St": [this.createRow()],
+				"Čt": [this.createRow()],
+				"Pá": [this.createRow()]
+			};
+		}
+
+		createRow(){
+			let newRow = [];
+			newRow.length = 14;
+			newRow.fill(false);
+			return newRow;
+		}
+
+
+		findSuitableRow(columStart,columnEnd,day){
+			//newRowNeeded = false;
+			columStart = columStart-2
+			columnEnd = columnEnd-2
+
+			for (let rowIndex = 0; rowIndex < this.rows[day].length; rowIndex++){
+				let suitable = true;
+				for(let spanIndex = columStart; spanIndex < columnEnd; spanIndex++){
+					if(this.rows[day][rowIndex][spanIndex]){
+						suitable = false
+						break;
+					}
+				}
+
+				if(suitable){
+					for(let spanIndex = columStart; spanIndex < columnEnd; spanIndex++){
+
+						//console.log(this.rows[day]);
+						//console.log(spanIndex);
+
+						this.rows[day][rowIndex][spanIndex] = true;
+					}
+					return rowIndex+1
+				}
+
+			}
+
+			this.rows[day].push(this.createRow());
+			for(let spanIndex = columStart; spanIndex < columnEnd; spanIndex++){
+				this.rows[day][this.rows[day].length-1][spanIndex] = true;
+			}
+			return this.rows[day].length;
+
+		}
+
+	}
+
+	const roc = new rowOffsetCounter();
+
 	function computeProperties(){
-		let tmp = [0,0,0,0,0,0,0,0,0,0,0,0,0]
 
 		for (let lectureIndex = 0; lectureIndex < lecturesData.length ;lectureIndex++){
 
@@ -17,17 +79,30 @@
 
 			for (let lectureType in lecture.terms){
 
+				if ("disabled" in lecture && lecture.disabled[lectureType]){
+					continue;
+				}
+
 				const terms = lecture.terms[lectureType];
 
 				for (let termIndex = 0; termIndex < terms.length; termIndex++){
 					const term = terms[termIndex]
-					let pos = parseInt(term.timeFrom) - 7;
+					const start = parseInt(term.timeFrom) - 5;
+					const end = parseInt(term.timeTo) - 4;
 
-					lecturesData[lectureIndex].terms[lectureType][termIndex].layout = {
-						"column": pos + 1
+					/*
+					for (let spanIndex = 0; i < start - end; spanIndex++){
+						spanIndex
 					}
 
-					//let span = parseInt(term.timeTo) - 6 - pos;
+					*/
+					//console.log(span);
+
+					lecturesData[lectureIndex].terms[lectureType][termIndex].layout = {
+						"columnStart": start,
+						"columnEnd": end,
+						"row": roc.findSuitableRow(start,end,term.day)
+					}
 
 
 				}
@@ -36,7 +111,7 @@
 
 		}
 
-	console.log(lecturesData);
+	//console.log(lecturesData);
 
 	}
 
@@ -50,6 +125,15 @@
   }
 */
 
+
+	function getProperties(term){
+		return `
+			grid-column-start: ${term.layout.columnStart};
+			grid-column-end: ${term.layout.columnEnd};
+			grid-row: ${term.layout.row};
+		`
+	}
+
 	function subjectHideHandler(e) {
 
 		if (!("disabled" in lecturesData[e.detail.subjectIndex])){
@@ -58,7 +142,10 @@
 
 		lecturesData[e.detail.subjectIndex].disabled[e.detail.termType] = !e.detail.value;
 
+		roc.clearRows();
 		computeProperties();
+		console.log(roc.rows["Po"]);
+		lecturesData = lecturesData;
 	}
 
 	seasonIndex.subscribe(async (index) => {
@@ -81,8 +168,9 @@
 		lecturesData = response.data
 		//console.log(lecturesData.terms);
 
+		roc.clearRows();
 		computeProperties();
-
+		console.log(roc.rows["Po"]);
 		subjectsLoading.set(false);
 	});
 
@@ -106,19 +194,37 @@
 		</div>
 
 		<div class="reg-form">
-			{#each lecturesData as lecture}
-
-				{#each Object.keys(lecture.terms) as lectureType}
-
-					{#if !("disabled" in lecture && lecture.disabled[lectureType])}
-						{#each lecture.terms[lectureType] as term}
-							<div class="term" style={"grid-column:" + term.layout.column}>
-							{lecture.abbr} {term.type}
-							</div>
-						{/each}
-					{/if}
-
+			<div class="time-header">
+				<div></div>
+				{#each Array(14) as _, index (index)}
+					<div class="time">{index+7}:00</div>
 				{/each}
+			</div>
+
+			{#each ["Po","Út","St","Čt","Pá"] as day}
+				<div class="reg-section">
+
+					<div class="day">
+					{day}
+					</div>
+
+					{#each lecturesData as lecture}
+
+						{#each Object.keys(lecture.terms) as lectureType}
+
+							{#if !("disabled" in lecture && lecture.disabled[lectureType])}
+								{#each lecture.terms[lectureType] as term}
+									{#if term.day == day}
+										<div class="term" style={getProperties(term)}>
+										{lecture.abbr} {term.type}
+										</div>
+									{/if}
+								{/each}
+							{/if}
+
+						{/each}
+					{/each}
+				</div>
 			{/each}
 		</div>
 
@@ -127,23 +233,41 @@
 </div>
 
 <style>
-.reg-form{
+.reg-form {
 	position: relative;
-	display: grid;
-	grid-template-columns: repeat(13, 1fr);
-	/*grid-template-areas: "a a a a a a a a a a a a a";*/
 	width: 1000px;
 	height: 100%;
 	border: solid 1px black;
 }
 
+.time-header {
+	display: grid;
+	grid-template-columns: 25px repeat(14, 76px);
+}
+
+.time {
+	border-left: 1px solid black
+}
+
+.reg-section {
+	display: grid;
+	grid-template-columns: 25px repeat(14, 76px);
+	width: 1000px;
+	height: 100%;
+	border-bottom: solid 1px red;
+}
+
+.day {
+	grid-column: 1;
+	justify-self: center;
+	border-right: green 1px solid;
+}
+
 .term{
 	position: relative;
-	width: 76px;
+	/*width: 76px;*/
 	height: 50px;
-	/*border: solid 1px green;*/
-	background: cyan;
-	grid-row: auto;
+	border: solid 1px black;
 
 }
 </style>
